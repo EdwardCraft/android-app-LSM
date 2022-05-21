09348c00f6ae24aca228c3da2b339d49fff2df5e
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2022-05-20 19:13:45 -0700

    created the project and  added the main screen

---
+++ app/src/main/java/com/example/lsm_app/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.lsm_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.lsm_app.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private CardView vowelsCard;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_about)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Listener on each of the cards
+        vowelsCard = findViewById(R.id.vowels_card);
+        vowelsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Test de listener",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/lsm_app/ui/listViews/ListViewViewModel.java
@@ -0,0 +1,7 @@
+package com.example.lsm_app.ui.listViews;
+
+import androidx.lifecycle.ViewModel;
+
+public class ListViewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
